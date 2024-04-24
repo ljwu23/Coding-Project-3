3733d5f110dfa2314e18daf0a35eaa3d81da0806
ljwu23 <lwu17@sfsu.edu>
2024-04-24 10:01:20 -0700

    Initial Commit

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -8,5 +8,14 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of vectorized vector-matrix multiply
-
+  __m256d y_vec, a_vec, x_vec;
+  for (int i = 0; i < n; i++) {
+      y_vec = _mm256_loadu_pd(&y[i]); // Load y[i] into y_vec
+      for (int j = 0; j < n; j += 4) {
+         a_vec = _mm256_loadu_pd(&A[i * n + j]); // Load four consecutive elements of A into a_vec
+         x_vec = _mm256_loadu_pd(&x[j]); // Load four consecutive elements of x into x_vec
+         y_vec = _mm256_fmadd_pd(a_vec, x_vec, y_vec); // Multiply-add operation: y_vec = a_vec * x_vec + y_vec
+      }
+      _mm256_storeu_pd(&y[i], y_vec); // Store the result back to y
+   }
 }
