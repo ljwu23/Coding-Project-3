1e6f97720f4bdc0c7f3c8c9d699ba3fa6eae0497
ljwu23 <lwu17@sfsu.edu>
2024-04-24 13:03:27 -0700

    Initial Commit

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -1,3 +1,4 @@
+#include <immintrin.h>
 const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
 
 /*
@@ -8,16 +9,13 @@ const char* dgemv_desc = "Vectorized implementation of matrix-vector multiply.";
  */
 void my_dgemv(int n, double* A, double* x, double* y) {
    // insert your code here: implementation of vectorized vector-matrix multiply
- for (int i = 0; i < n; i++) {
-      __m256d ymm_result = _mm256_setzero_pd(); // Initialize result register to zero
-      for (int j = 0; j < n; j += 4) { // Process 4 elements at a time
-         __m256d ymm_A = _mm256_loadu_pd(&A[i * n + j]); // Load 4 elements from A
-         __m256d ymm_x = _mm256_loadu_pd(&x[j]); // Load 4 elements from x
-         ymm_result = _mm256_add_pd(ymm_result, _mm256_mul_pd(ymm_A, ymm_x)); // Multiply and add
+   for (int i = 0; i < n; i++) {
+      __m256d result = _mm256_setzero_pd(); 
+      for (int j = 0; j < n; j += 4) { 
+         __m256d A = _mm256_loadu_pd(&A[i * n + j]); 
+         __m256d x = _mm256_loadu_pd(&x[j]); 
+         result = _mm256_fmadd_pd(A, x, result); 
       }
-      // Horizontal sum of the 4 elements in ymm_result
-      double result[4];
-      _mm256_storeu_pd(result, ymm_result);
       y[i] += result[0] + result[1] + result[2] + result[3];
    }
 }
