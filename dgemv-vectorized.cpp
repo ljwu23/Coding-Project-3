8118770610f2d066ca07018b2afe058451536517
ljwu23 <lwu17@sfsu.edu>
2024-04-24 13:07:13 -0700

    Initial Commit

--- dgemv-vectorized.cpp
+++ dgemv-vectorized.cpp
@@ -12,9 +12,9 @@ void my_dgemv(int n, double* A, double* x, double* y) {
    for (int i = 0; i < n; i++) {
       __m256d result = _mm256_setzero_pd(); 
       for (int j = 0; j < n; j += 4) { 
-         __m256d A = _mm256_loadu_pd(&A[i * n + j]); 
-         __m256d x = _mm256_loadu_pd(&x[j]); 
-         result = _mm256_fmadd_pd(A, x, result); 
+         __m256d ymm_A = _mm256_loadu_pd(&A[i * n + j]); 
+         __m256d ymm_x = _mm256_loadu_pd(&x[j]); 
+         result = _mm256_fmadd_pd(ymm_A, ymm_x, result); 
       }
       y[i] += result[0] + result[1] + result[2] + result[3];
    }
